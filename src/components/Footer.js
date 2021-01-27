67013138f4737849af9c9f9ec9e45fc9b33cd403
Cheakimse Mao <cheakimsemao@Cheakimses-MacBook-Pro.local>
2021-01-27 23:21:55 +0700

    added header & footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/footer.css';
+
+const Footer = () => {
+    return (
+        <div id='footer'>
+            <p className='blue'>
+                Created By <span className='yellow'>Team3</span>
+            </p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/App.css';
+import Header from './Header';
+import Footer from './Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header />
+            <p>Hello, World!</p>
+            <Footer />
+        </div>
+    );
+};
+
+export default Main;
