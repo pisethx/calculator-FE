90690227383220fbf8f6159087da7036c0e4acb7
Cheakimse Mao <cheakimsemao@Cheakimses-MacBook-Pro.local>
2021-02-01 18:59:46 +0700

    added responsive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
 import '../css/header.css';
 import { Link } from 'react-router-dom';
-import { AiFillCaretDown } from 'react-icons/ai';
-import { MdAccountCircle } from 'react-icons/md';
+import { AiFillCaretDown, AiOutlineClose } from 'react-icons/ai';
+import { MdAccountCircle, MdExpandMore } from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi';
 
 const Header = () => {
     const [showMenu, setShowMenu] = useState(false);
+    const [showMenuSide, setShowMenuSide] = useState(false);
     const drop = useRef(null);
 
     const handleClick = (e) => {
@@ -14,39 +16,40 @@ const Header = () => {
         }
     };
 
-    useEffect(() => {
-        document.addEventListener('click', handleClick);
-        return () => {
-            document.removeEventListener('click', handleClick);
-        };
-    });
+    // useEffect(() => {
+    //     document.addEventListener('click', handleClick);
+    //     return () => {
+    //         document.removeEventListener('click', handleClick);
+    //     };
+    // });
 
     return (
-        <div id='header'>
-            <div className='navbar blue'>
-                <Link to='/' className='brand'>
-                    KASHIO
-                </Link>
-                <div className='dropdown'>
-                    <button type='button' className='dropdown-btn'>
-                        Calculator
-                        <AiFillCaretDown className='caret-icon' />
-                    </button>
-                    <div className='dropdown-content display-none'>
-                        <Link to='/' className='dropdown-link'>
-                            Simple Calculator
-                        </Link>
-                        <hr />
-                        <Link to='/' className='dropdown-link'>
-                            Scientific Calculator
-                        </Link>
-                        <hr />
+        <>
+            <div id='header'>
+                <div id='left-navbar' className='navbar blue'>
+                    <Link to='/' className='brand'>
+                        KASHIO
+                    </Link>
+                    <div className='dropdown'>
+                        <button type='button' className='dropdown-btn'>
+                            Calculator
+                            <AiFillCaretDown className='caret-icon' />
+                        </button>
+                        <div className='dropdown-content display-none'>
+                            <Link to='/simple-calculator' className='dropdown-link'>
+                                Simple Calculator
+                            </Link>
+                            <hr />
+                            <Link to='/scientific-calculator' className='dropdown-link'>
+                                Scientific Calculator
+                            </Link>
+                            <hr />
+                        </div>
                     </div>
-                </div>
-                <Link to='/' className='link'>
-                    Unit Converter
-                </Link>
-                {/* <div className='dropdown'>
+                    <Link to='/' className='link'>
+                        Unit Converter
+                    </Link>
+                    {/* <div className='dropdown'>
                     <button className='dropdown-btn'>
                         Randomizer
                         <AiFillCaretDown className='caret-icon' />
@@ -74,39 +77,123 @@ const Header = () => {
                         <hr />
                     </div>
                 </div> */}
+                </div>
+                <div id='right-navbar'>
+                    {/* <button
+                        type='button'
+                        ref={drop}
+                        className='dropdown-btn no-pr-navbar'
+                        onClick={() => setShowMenu((showMenu) => !showMenu)}>
+                        <MdAccountCircle className='account-icon' />
+                    </button>
+                    {showMenu && (
+                        <div className='account-content'>
+                            <Link to='/' className='dropdown-link'>
+                                Cheakimse
+                            </Link>
+                            <hr />
+                            <Link to='/' className='dropdown-link'>
+                                Save List
+                            </Link>
+                            <hr />
+                            <Link to='/' className='dropdown-link'>
+                                Logout
+                            </Link>
+                            <hr />
+                        </div>
+                    )} */}
+                    <Link to='/' className='link'>
+                        Login
+                    </Link>
+                    <Link to='/' className='link no-pr-navbar'>
+                        Registration
+                    </Link>
+                    {/* menu-side */}
+                    <button
+                        type='button'
+                        id='menu-icon'
+                        className='dropdown-btn no-pr-navbar display-none'
+                        onClick={() => setShowMenuSide((showMenuSide) => !showMenuSide)}>
+                        <FiMenu className='menu-icon' />
+                    </button>
+                </div>
             </div>
-            <div>
-                {/* <button
-                    type='button'
-                    ref={drop}
-                    className='dropdown-btn no-pr-navbar'
-                    onClick={() => setShowMenu((showMenu) => !showMenu)}>
-                    <MdAccountCircle className='account-icon' />
-                </button>
-                {showMenu && (
-                    <div className='account-content'>
-                        <Link to='/' className='dropdown-link'>
-                            Cheakimse Mao
+            {showMenuSide && (
+                <div className='menu-side'>
+                    <button
+                        type='button'
+                        className='dropdown-btn'
+                        onClick={() => setShowMenuSide((showMenuSide) => !showMenuSide)}>
+                        <AiOutlineClose className='close-icon' />
+                    </button>
+                    {/* <div>
+                        <MdAccountCircle className='menu-account-icon' />
+                        <p className='username'>Cheakimse</p>
+                    </div> */}
+                    <div className='menu-side-content'>
+                        <div className='menu-dropdown'>
+                            <button type='button' className='menu-side-btn'>
+                                Calculator
+                                <MdExpandMore className='expand-icon' />
+                            </button>
+                            <div className='sub-menu display-none'>
+                                <Link to='/simple-calculator' className='menu-side-link no-mt-menu-side'>
+                                    Simple Calculator
+                                </Link>
+                                <Link to='/scientific-calculator' className='menu-side-link'>
+                                    Scientific Calculator
+                                </Link>
+                            </div>
+                        </div>
+                        <hr />
+                        <Link to='/' className='menu-side-link'>
+                            Unit Converter
                         </Link>
                         <hr />
-                        <Link to='/' className='dropdown-link'>
+                        <Link to='/' className='menu-side-link'>
+                            Login
+                        </Link>
+                        <hr />
+                        <Link to='/' className='menu-side-link'>
+                            Registration
+                        </Link>
+                        <hr />
+                        {/* <div className='menu-dropdown'>
+                            <button type='button' className='menu-side-btn'>
+                                Randomizer
+                                <MdExpandMore className='expand-icon' />
+                            </button>
+                            <div className='sub-menu display-none'>
+                                <Link to='/' className='menu-side-link no-mt-menu-side'>
+                                    Team Generator
+                                </Link>
+                                <Link to='/' className='menu-side-link'>
+                                    Name Picker
+                                </Link>
+                                <Link to='/' className='menu-side-link'>
+                                    Yes or No
+                                </Link>
+                                <Link to='/' className='menu-side-link'>
+                                    Decision Maker
+                                </Link>
+                                <Link to='/' className='menu-side-link'>
+                                    Random Picker
+                                </Link>
+                            </div>
+                            <hr />
+                        </div>
+                        <Link to='/' className='menu-side-link'>
                             Save List
                         </Link>
                         <hr />
-                        <Link to='/' className='dropdown-link'>
+                        <Link to='/' className='menu-side-link'>
                             Logout
                         </Link>
-                        <hr />
+                        <hr /> */}
                     </div>
-                )} */}
-                <Link to='/' className='link'>
-                    Login
-                </Link>
-                <Link to='/' className='link no-pr-navbar'>
-                    Registration
-                </Link>
-            </div>
-        </div>
+                </div>
+            )}
+        </>
     );
 };
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,19 @@ import React from 'react';
 import '../css/App.css';
 import Header from './Header';
 import Footer from './Footer';
+// import SimpleCalculator from './SimpleCalculator';
+// import ScientificCalculator from './ScientificCalculator';
+import { Switch, Route } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
             <Header />
-            <p>Hello, World!</p>
+            {/* <Switch>
+                <Route exact path='/' component={SimpleCalculator} />
+                <Route path='/simple-calculator' component={SimpleCalculator} />
+                <Route path='/scientific-calculator' component={ScientificCalculator} />
+            </Switch> */}
             <Footer />
         </div>
     );
