facdd25e9e8659d3b9b6beb00d066c63e6971fa6
Cheakimse Mao <cheakimsemao@Cheakimses-MacBook-Pro.local>
2021-02-19 17:11:05 +0700

    fixed regex

--- src/components/ScientificCalculator.js
+++ src/components/ScientificCalculator.js
@@ -41,17 +41,20 @@ class AutoScalingText extends Component {
 class CalculatorDisplay extends Component {
     render() {
         const { value, ...props } = this.props;
-
         const language = navigator.language || 'en-US';
-        let formattedValue = parseFloat(value).toLocaleString(language, {
+        const escapedKeys = [' e ', '(', ')'];
+        let isNumeric = true;
+
+        escapedKeys.map((key) => {
+            if (value.includes(key)) isNumeric = false;
+        });
+
+        const parsedValue = isNumeric ? parseFloat(value) : value;
+
+        let formattedValue = parsedValue.toLocaleString(language, {
             useGrouping: true,
             maximumFractionDigits: 10,
         });
-
-        // Add back missing .0 in e.g. 12.0
-        const match = value.match(/\.\d*?(0*)$/);
-
-        if (match) formattedValue += /[1-9]/.test(match[0]) ? match[1] : match[0];
 
         return (
             <div {...props}>
